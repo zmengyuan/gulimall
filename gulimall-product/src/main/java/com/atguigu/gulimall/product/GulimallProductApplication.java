4ad742266bd9ef3ae0c951d12a28d9d2a7914134
zmengyuan <zhangmy5@asiainfo.com>
2020-12-21 23:21:30 +0800

    [add]添加一些mybatis-plus配置，测试service

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,36 @@
 package com.atguigu.gulimall.product;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+1、整合mybatis-plus
+    1)导入依赖
+    <dependency>
+            <groupId>com.baomidou</groupId>
+            <artifactId>mybatis-plus-boot-starter</artifactId>
+            <version>${mybatisplus.version}</version>
+        </dependency>
+    2）配置
+        1、数据源
+            1)数据库驱动
+            <!-- 导入mysql 驱动-->
+    <!-- https://mvnrepository.com/artifact/mysql/mysql-connector-java -->
+    <dependency>
+      <groupId>mysql</groupId>
+      <artifactId>mysql-connector-java</artifactId>
+      <version>${mysql.version}</version>
+    </dependency>
+            2)配置application.yml中的数据库
+        2、配置mybatis-plus
+            1） 使用@MapperScan()
+            2） 告诉mybatis-plus,xml在哪里
+            mybatis-plus:
+                mapper-location
+ */
+@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
