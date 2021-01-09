949814f8b76e2df6246f1f637b3d39e97e1a5f52
zmengyuan <zhangmy5@asiainfo.com>
2021-01-09 18:06:54 +0800

    [add]单元测试第三方上传

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,9 +1,5 @@
 package com.atguigu.gulimall.product;
 
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.model.PutObjectRequest;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.Test;
@@ -23,18 +19,7 @@ class GulimallProductApplicationTests {
     @Autowired
     BrandService brandService;
 
-    @Autowired
-    OSSClient ossClient;
 
-    @Test
-    void testUploadByAlicloudOss() throws FileNotFoundException {
-        String bucketName = "passjava";
-        String localFile = "C:\\Users\\Administrator\\Pictures\\coding_java.png";
-        String fileKeyName = "coding_java.png";
-        InputStream inputStream = new FileInputStream(localFile);
-        ossClient.putObject(bucketName, fileKeyName, inputStream);
-        ossClient.shutdown();
-    }
 
 
     @Test
@@ -45,31 +30,6 @@ class GulimallProductApplicationTests {
         System.out.println("保存成功");
     }
 
-    @Test
-    void upload() {
-        // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
-// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "<yourAccessKeyId>";
-        String accessKeySecret = "<yourAccessKeySecret>";
 
-// 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-
-// 创建PutObjectRequest对象。
-        PutObjectRequest putObjectRequest = new PutObjectRequest("<yourBucketName>", "<yourObjectName>", new File("<yourLocalFile>"));
-
-// 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
-// ObjectMetadata metadata = new ObjectMetadata();
-// metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
-// metadata.setObjectAcl(CannedAccessControlList.Private);
-// putObjectRequest.setMetadata(metadata);
-
-// 上传文件。
-        ossClient.putObject(putObjectRequest);
-
-// 关闭OSSClient。
-        ossClient.shutdown();
-    }
 
 }
--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,11 +1,59 @@
 package com.atguigu.gulimall.thirdparty;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.PutObjectRequest;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class GulimallThirdPartyApplicationTests {
 
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+    void testUploadByAlicloudOss() throws FileNotFoundException {
+        String bucketName = "passjava";
+        String localFile = "C:\\Users\\Administrator\\Pictures\\coding_java.png";
+        String fileKeyName = "coding_java.png";
+        InputStream inputStream = new FileInputStream(localFile);
+        ossClient.putObject(bucketName, fileKeyName, inputStream);
+        ossClient.shutdown();
+    }
+    @Test
+    void upload() {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "<yourAccessKeyId>";
+        String accessKeySecret = "<yourAccessKeySecret>";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 创建PutObjectRequest对象。
+        PutObjectRequest putObjectRequest = new PutObjectRequest("<yourBucketName>", "<yourObjectName>", new File("<yourLocalFile>"));
+
+// 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
+// ObjectMetadata metadata = new ObjectMetadata();
+// metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+// metadata.setObjectAcl(CannedAccessControlList.Private);
+// putObjectRequest.setMetadata(metadata);
+
+// 上传文件。
+        ossClient.putObject(putObjectRequest);
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+    }
     @Test
     void contextLoads() {
     }
