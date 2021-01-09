8b72723e263513eac434692545bd5ff10d3e8955
zmengyuan <zhangmy5@asiainfo.com>
2021-01-09 17:23:02 +0800

    [add]使用原生sdk上传阿里oss

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,25 +1,57 @@
-//package com.atguigu.gulimall.product;
-//
-//import com.atguigu.gulimall.product.entity.BrandEntity;
-//import com.atguigu.gulimall.product.service.BrandService;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//class GulimallProductApplicationTests {
-//    @Autowired
-//    BrandService brandService;
-//
-//    @Test
-//    void contextLoads() {
-//        BrandEntity c = new BrandEntity();
-//        c.setName("华为");
-//        brandService.save(c);
-//        System.out.println("保存成功");
-//    }
-//
-//}
+package com.atguigu.gulimall.product;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class GulimallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
+
+    @Test
+    void contextLoads() {
+        BrandEntity c = new BrandEntity();
+        c.setName("华为");
+        brandService.save(c);
+        System.out.println("保存成功");
+    }
+
+    @Test
+    void upload() {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "<yourAccessKeyId>";
+        String accessKeySecret = "<yourAccessKeySecret>";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 创建PutObjectRequest对象。
+        PutObjectRequest putObjectRequest = new PutObjectRequest("<yourBucketName>", "<yourObjectName>", new File("<yourLocalFile>"));
+
+// 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
+// ObjectMetadata metadata = new ObjectMetadata();
+// metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+// metadata.setObjectAcl(CannedAccessControlList.Private);
+// putObjectRequest.setMetadata(metadata);
+
+// 上传文件。
+        ossClient.putObject(putObjectRequest);
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+    }
+
+}
