a50aa15e1c6ce7abacc70447b4e200320d87ac9f
zmengyuan <zhangmy5@asiainfo.com>
2021-01-09 21:25:24 +0800

    [add]商品服务-查询三级分类的属性分组

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -11,6 +11,7 @@ import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.product.dao.AttrGroupDao;
 import com.atguigu.gulimall.product.entity.AttrGroupEntity;
 import com.atguigu.gulimall.product.service.AttrGroupService;
+import org.springframework.util.StringUtils;
 
 
 @Service("attrGroupService")
@@ -26,4 +27,27 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         return new PageUtils(page);
     }
 
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        if (null == catelogId) {
+            IPage<AttrGroupEntity> page = this.page(new Query<AttrGroupEntity>().getPage(params)
+                    , new QueryWrapper<AttrGroupEntity>());
+            return new PageUtils(page);
+        }else {
+            // select * from pms_attr_group where category_id =  and  (字段1=key or 字段2   =）
+            String key = (String) params.get("key");
+            //构造查询条件
+            QueryWrapper<AttrGroupEntity> wrapper = new QueryWrapper<AttrGroupEntity>()
+                    .eq("catelog_id",catelogId);
+            if (!StringUtils.isEmpty(key)) {
+
+                wrapper.and((obj) -> {
+                    obj.eq("attr_group_id",key).or().like("attr_group_name",key);
+                });
+            }
+            IPage<AttrGroupEntity> page = this.page(new Query<AttrGroupEntity>().getPage(params),wrapper);
+            return new PageUtils(page);
+        }
+
+    }
 }
