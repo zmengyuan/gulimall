f078aed4df1ff0657ffa3b3f149e659f6db1708b
zmengyuan <zhangmy5@asiainfo.com>
2021-01-12 11:26:01 +0800

    [add]88商品服务- 1、新增商品，spu基本信息（未测试）

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SpuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SpuInfoServiceImpl.java
@@ -1,8 +1,20 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.entity.AttrEntity;
+import com.atguigu.gulimall.product.entity.ProductAttrValueEntity;
+import com.atguigu.gulimall.product.entity.SpuInfoDescEntity;
+import com.atguigu.gulimall.product.service.*;
+import com.atguigu.gulimall.product.vo.BaseAttrs;
 import com.atguigu.gulimall.product.vo.SpuSaveVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,12 +23,23 @@ import com.atguigu.common.utils.Query;
 
 import com.atguigu.gulimall.product.dao.SpuInfoDao;
 import com.atguigu.gulimall.product.entity.SpuInfoEntity;
-import com.atguigu.gulimall.product.service.SpuInfoService;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("spuInfoService")
 public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> implements SpuInfoService {
+    @Autowired
+    SpuInfoDescService spuInfoDescService;
+
+    @Autowired
+    SpuImagesService imagesService;
+
+    @Autowired
+    AttrService attrService;
+
+    @Autowired
+    ProductAttrValueService attrValueService;
+
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -35,12 +58,42 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
     @Override
     public void saveSpuInfo(SpuSaveVo vo) {
         //1、保存spu基本信息 pms_spu_info
+        SpuInfoEntity infoEntity = new SpuInfoEntity();
+        BeanUtils.copyProperties(vo,infoEntity);
+        infoEntity.setCreateTime(new Date());
+        infoEntity.setUpdateTime(new Date());
+        this.saveBaseSpuInfo(infoEntity);
+
 
         //2、保存Spu的描述图片 pms_spu_info_desc
+        List<String> decript = vo.getDecript();
+        SpuInfoDescEntity descEntity = new SpuInfoDescEntity();
+        descEntity.setSpuId(infoEntity.getId());
+        descEntity.setDecript(String.join(",",decript));
+        spuInfoDescService.saveSpuInfoDesc(descEntity);
+
 
         //3、保存spu的图片集 pms_spu_images
+        List<String> images = vo.getImages();
+        imagesService.saveImages(infoEntity.getId(),images);
+
 
         //4、保存spu的规格参数;pms_product_attr_value
+        List<BaseAttrs> baseAttrs = vo.getBaseAttrs();
+        List<ProductAttrValueEntity> collect = baseAttrs.stream().map(attr -> {
+            ProductAttrValueEntity valueEntity = new ProductAttrValueEntity();
+            valueEntity.setAttrId(attr.getAttrId());
+            AttrEntity id = attrService.getById(attr.getAttrId());
+            valueEntity.setAttrName(id.getAttrName());
+            valueEntity.setAttrValue(attr.getAttrValues());
+            valueEntity.setQuickShow(attr.getShowDesc());
+            valueEntity.setSpuId(infoEntity.getId());
+
+            return valueEntity;
+        }).collect(Collectors.toList());
+        attrValueService.saveProductAttr(collect);
+
+
 
         //5、保存spu的积分信息；gulimall_sms->sms_spu_bounds
 
@@ -49,5 +102,10 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
         //5.2）、sku的图片信息；pms_sku_image
         //5.3）、sku的销售属性信息：pms_sku_sale_attr_value
         //5.4）、sku的优惠、满减等信息；gulimall_sms->sms_sku_ladder打折\sms_sku_full_reduction满减\sms_member_price会员价格
+    }
+
+    @Override
+    public void saveBaseSpuInfo(SpuInfoEntity infoEntity) {
+        this.baseMapper.insert(infoEntity);
     }
 }
