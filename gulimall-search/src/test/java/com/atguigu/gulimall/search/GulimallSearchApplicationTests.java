6843cf86a3fc849a4ae2b17fa6b091a71bec62f2
zmengyuan <zhangmy5@asiainfo.com>
2021-01-15 14:14:48 +0800

    [add]126es新增数据

--- gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,21 +1,56 @@
 package com.atguigu.gulimall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.gulimall.search.config.GulimallElasticSearchConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GulimallSearchApplicationTests {
 
     @Autowired
-    RestHighLevelClient restHighLevelClient;
+    RestHighLevelClient client;
     @Test
     public void contextLoads() {
-        System.out.println(restHighLevelClient);
+        System.out.println(client);
+    }
+
+    @Test
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest ("users");
+        indexRequest.id("1");//数据的id
+//        indexRequest.source("username","zhangsan","age",18,"gender","男");  //第一种方式
+
+        User user = new User();
+        user.setUserName("张三");
+        user.setAge(20);
+        user.setGender("男");
+        String jsonString = JSON.toJSONString(user);
+        //设置要保存的内容
+        indexRequest.source(jsonString, XContentType.JSON);
+        //执行创建索引和保存数据
+        IndexResponse index = client.index(indexRequest, RequestOptions.DEFAULT);
+
+        System.out.println(index);
+
+    }
+    @Data
+    class User{
+        private String userName;
+        private String gender;
+        private Integer age;
     }
 
 }
