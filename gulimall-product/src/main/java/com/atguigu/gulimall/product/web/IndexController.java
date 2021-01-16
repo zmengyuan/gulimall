d6a1d69e59e5355c13cbfaf112fd9af080a9888e
zmengyuan <zhangmy5@asiainfo.com>
2021-01-16 11:38:03 +0800

    [add]137  页面一级分类

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -20,8 +20,8 @@ public class IndexController {
     @GetMapping({"/", "index.html"})
     public String getIndex(Model model) {
         //获取所有的一级分类
-        List<CategoryEntity> catagories = categoryService.getLevel1Catagories();
-        model.addAttribute("catagories", catagories);
+        List<CategoryEntity> categories = categoryService.getLevel1Catagories();
+        model.addAttribute("categories", categories);
         // 视图解析器进行拼串
         // classpath:/templates/+返回值+   .html
         return "index";
