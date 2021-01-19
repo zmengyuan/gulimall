98113da74ec7f6e0326bb2dbbd075d0732599aa2
zmengyuan <zhangmy5@asiainfo.com>
2021-01-19 15:20:32 +0800

    [add]164 redission的闭锁 CountDownLatch

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -4,6 +4,7 @@ import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.CategoryService;
 import com.atguigu.gulimall.product.vo.Catalog2Vo;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RCountDownLatch;
 import org.redisson.api.RLock;
 import org.redisson.api.RReadWriteLock;
 import org.redisson.api.RedissonClient;
@@ -12,6 +13,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -105,5 +108,31 @@ public class IndexController {
             lock.unlock();
         }
         return uuid;
+    }
+
+    /**
+     * 放假，锁门
+     * 1班没人了
+     * 5个班全部走完，我们可以锁大门
+     */
+    @RequestMapping(value = "/lockDoor")
+    @ResponseBody
+    public String lockDoor() {
+        RCountDownLatch door = redisson.getCountDownLatch("door");
+        door.trySetCount(5L);
+        try {
+            door.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "放假了";
+    }
+
+    @RequestMapping(value = "/gogogo/{id}")
+    @ResponseBody
+    public String gogogo(@PathVariable("id") Long id) {
+        RCountDownLatch door = redisson.getCountDownLatch("door");
+        door.countDown();
+        return id+"班的人走了";
     }
 }
