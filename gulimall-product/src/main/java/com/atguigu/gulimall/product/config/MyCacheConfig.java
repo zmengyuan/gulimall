b231a621a17a1fbb56ca54e88f0943dca14c0b45
zmengyuan <zhangmy5@asiainfo.com>
2021-01-20 11:07:01 +0800

    [add]170 使得application.yml中的参数起作用。

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/config/MyCacheConfig.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/config/MyCacheConfig.java
@@ -1,20 +1,54 @@
 package com.atguigu.gulimall.product.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
+@EnableConfigurationProperties(CacheProperties.class)
 @Configuration
 public class MyCacheConfig {
+    /**
+     * 配置文件的东西没有用到
+     * 1、原来和配置文件绑定的配置类是这样的
+     * @ConfigurationProperties(prefix="spring.cache")
+     * public class CacheProperties
+     * 但是只要没有放到容器中都是不生效的。
+     *
+     * 2、让他生效
+     * @EnableConfigurationProperties(CacheProperties.class)
+     * @return
+     */
+    @Autowired
+    CacheProperties cacheProperties;
+
     @Bean
     RedisCacheConfiguration redisCacheConfiguration() {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
 
         config = config.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
         config = config.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        //将配置文件中的所有配置都生效
+        CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+        //设置配置文件中的各项配置，如过期时间
+        if (redisProperties.getTimeToLive() != null) {
+            config = config.entryTtl(redisProperties.getTimeToLive());
+        }
+        if (redisProperties.getKeyPrefix() != null) {
+            config = config.prefixKeysWith(redisProperties.getKeyPrefix());
+        }
+        if (!redisProperties.isCacheNullValues()) {
+            config = config.disableCachingNullValues();
+        }
+        if (!redisProperties.isUseKeyPrefix()) {
+            config = config.disableKeyPrefix();
+        }
         return config;
+
     }
 }
