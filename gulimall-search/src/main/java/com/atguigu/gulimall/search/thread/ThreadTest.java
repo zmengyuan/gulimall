d92ad88f938531511466d652b1b4a20cf71598f5
zmengyuan <zhangmy5@asiainfo.com>
2021-01-23 17:02:54 +0800

    [add]196-197 whenComplete

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/thread/ThreadTest.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/thread/ThreadTest.java
@@ -16,6 +16,14 @@ public class ThreadTest {
     public static CompletableFuture<Void> runAsync(Runnable runnable, Executor executor)//指定线程池
     public static <U> CompletableFuture<U> supplyAsync(Supplier<U> supplier)//有返回值
     public static <U> CompletableFuture<U> supplyAsync(Supplier<U> supplier, Executor executor)
+
+    当前一个任务执行完成，可以继续后面写代码。或者使用CompletableFuture提供的以下方法
+    //可以处理异常，无返回值
+    public CompletableFuture<T> whenComplete(BiConsumer<? super T,? super Throwable> action)//使用前一个相同线程执行
+    public CompletableFuture<T> whenCompleteAsync(BiConsumer<? super T,? super Throwable> action)//前一个成功以后，这个任务还是以异步的方式执行，交给线程池执行
+    public CompletableFuture<T> whenCompleteAsync(BiConsumer<? super T,? super Throwable> action, Executor executor)
+    //可以处理异常，有返回值
+    public CompletableFuture<T> exceptionally(Function<Throwable,? extends T> fn)
      */
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.out.println("方法开始");
@@ -25,15 +33,30 @@ public class ThreadTest {
             System.out.println("当前运行结果:" + i);
         }, service);*/
 
-        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+        /*CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             System.out.println("当前线程：" + Thread.currentThread().getId());
             int i = 10 / 2;
             System.out.println("当前运行结果:" + i);
             return i;
         }, service);
         Integer i = future.get();
-        System.out.println(i);
-        System.out.println("方法结束");
+        System.out.println(i);*/
+
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("当前线程：" + Thread.currentThread().getId());
+            int i = 10 / 0;
+            System.out.println("当前运行结果:" + i);
+            return i;
+        }, service).whenComplete((t,v) -> {
+            //t 是结果  v是异常  虽然能知道异常信息，但是没法修改异常数据
+            System.out.println("打印异步任务成功后的t:{"+t+"}，v是：{"+v+"}");
+        }).exceptionally(throww -> {
+            //可以感知异常，并且处理异常返回
+            return 10;
+        });
+        Integer f = future.get();
+
+        System.out.println("方法结束"+f);
     }
 
 
