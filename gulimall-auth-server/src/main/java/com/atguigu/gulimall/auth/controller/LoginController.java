f1ac92259a86112cd2da6e951d3526e0d407f68f
zmengyuan <zhangmy5@asiainfo.com>
2021-01-24 11:47:26 +0800

    [add]212 viewController视图

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
@@ -3,8 +3,12 @@ package com.atguigu.gulimall.auth.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+//@Controller
 public class LoginController {
+    /*
+    发送一个请求直接跳转到页面
+    SpringMVC viewController 将请求和页面映射过来
+     */
 
     @GetMapping("/login.html")
     public String loginPage(){
