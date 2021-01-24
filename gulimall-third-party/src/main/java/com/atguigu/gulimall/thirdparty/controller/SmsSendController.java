0e97b1d1adb2e32a15af5924e08dfdc4abcc05f5
zmengyuan <zhangmy5@asiainfo.com>
2021-01-24 12:06:04 +0800

    [add]214 三方服务开通一个短信发送服务，本服务直接一直返回true

---
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/SmsSendController.java
@@ -0,0 +1,29 @@
+package com.atguigu.gulimall.thirdparty.controller;
+
+import com.atguigu.common.utils.R;
+import com.atguigu.gulimall.thirdparty.component.SmsComponent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sms")
+public class SmsSendController {
+
+    @Autowired
+    SmsComponent smsComponent;
+    /**
+     * 提供给别的服务进行调用。不是给页面调用的
+     * @param phone
+     * @param code
+     * @return
+     */
+    @GetMapping("/sendcode")
+    public R sendCode(@RequestParam("phone") String phone, @RequestParam("code") String code){
+        //TODO 本项目没有调试，直接返回true
+//        smsComponent.sendSmsCode(phone,code);
+        return R.ok();
+    }
+}
