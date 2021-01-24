76a8d2f7aa4feb268b979c81a28594169ce6b939
zmengyuan <zhangmy5@asiainfo.com>
2021-01-24 13:04:25 +0800

    [add]214 点击发送验证码逻辑ok

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
@@ -1,22 +1,45 @@
 package com.atguigu.gulimall.auth.controller;
 
+import com.atguigu.common.constant.AuthServerConstant;
+import com.atguigu.common.exception.BizCodeEnum;
 import com.atguigu.common.utils.R;
 import com.atguigu.gulimall.auth.feign.ThirdPartFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.TimeUnit;
+
 @Controller
 public class LoginController {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
     @Autowired
     ThirdPartFeignService thirdPartFeignService;
 
     @ResponseBody
     @GetMapping("/sms/sendcode")
     public R sendCode(@RequestParam("phone") String phone){
+        //1、接口防刷
+        String prefixPhone = AuthServerConstant.SMS_CODE_CACHE_PREFIX + phone;
+        String redisCode = stringRedisTemplate.opsForValue().get(prefixPhone);
+        if (!StringUtils.isEmpty(redisCode)){
+            long l = Long.parseLong(redisCode.split("_")[1]);
+            if (System.currentTimeMillis() -l < 60000){
+                //60秒内不能再发
+                return R.error(BizCodeEnum.SMS_CODE_EXCEPTION.getCode(),BizCodeEnum.SMS_CODE_EXCEPTION.getMsg());
+            }
+        }
+
+        //2、验证码再次验证 redis
         String code = String.valueOf((int)((Math.random() + 1) * 100000));
+        //redis缓存验证码   防止同一个phone在60s内再次发送验证码  set(K var1, V var2, long var3, TimeUnit var5)
+        stringRedisTemplate.opsForValue().set(prefixPhone,code + "_" + System.currentTimeMillis(),10, TimeUnit.MINUTES);
         thirdPartFeignService.sendCode(phone,code);
         return R.ok();
     }
---
+++ gulimall-common/src/main/java/com/atguigu/common/constant/AuthServerConstant.java
@@ -0,0 +1,6 @@
+package com.atguigu.common.constant;
+
+public class AuthServerConstant {
+    //短信注册验证码 缓存key前缀
+    public static final String SMS_CODE_CACHE_PREFIX = "sms:code:";
+}
