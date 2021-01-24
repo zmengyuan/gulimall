2b18e31ee0a02cd6e4102a2393037ba4d4e0ac70
zmengyuan <zhangmy5@asiainfo.com>
2021-01-24 14:17:19 +0800

    [add]215 点击立即注册回显错误

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
@@ -4,15 +4,23 @@ import com.atguigu.common.constant.AuthServerConstant;
 import com.atguigu.common.exception.BizCodeEnum;
 import com.atguigu.common.utils.R;
 import com.atguigu.gulimall.auth.feign.ThirdPartFeignService;
+import com.atguigu.gulimall.auth.vo.UserRegistVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Controller
 public class LoginController {
@@ -42,5 +50,35 @@ public class LoginController {
         stringRedisTemplate.opsForValue().set(prefixPhone,code + "_" + System.currentTimeMillis(),10, TimeUnit.MINUTES);
         thirdPartFeignService.sendCode(phone,code);
         return R.ok();
+    }
+
+    @PostMapping("/regist")
+    public String regist(@Valid UserRegistVo vo, BindingResult result,
+                         Model model){
+        if (result.hasErrors()) {
+            Map<String, String> errors =result.getFieldErrors().stream().collect(Collectors.toMap(fieldError -> {
+                 return fieldError.getField();
+             },(fieldError) ->{
+                 return fieldError.getDefaultMessage();
+             }));
+            model.addAttribute("errors",errors);
+
+
+            /**
+             * 使用 return "forward:/reg.html"; 会出现
+             * 问题：Request method 'POST' not supported的问题
+             * 原因：用户注册-> /regist[post] ------>转发/reg.html (路径映射默认都是get方式访问的)
+             * 校验出错转发到注册页。
+             */
+//            return "forward:/reg.html";//这种是重新走viewController
+//            转发模式会造成重复提交表单的问题，所以改成重定向
+//            return "reg";//这种是转发直接到视图
+//            使用重定向  解决重复提交的问题。但面临着数据不能携带的问题，就用RedirectAttributes
+            return "redirect:http://auth.gulimall.com/reg.html";
+        }
+
+
+
+        return "redirect:http://auth.gulimall.com/login.html";
     }
 }
