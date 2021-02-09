f0640c9d784315f269c401ef65935d12cff1f5c7
zmengyuan <zhangmy5@asiainfo.com>
2021-02-09 22:32:57 +0800

    [add] 订单服务 273 订单确认页

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareInfoController.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareInfoController.java
@@ -3,12 +3,9 @@ package com.atguigu.gulimall.ware.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.atguigu.gulimall.ware.vo.FareVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.gulimall.ware.entity.WareInfoEntity;
 import com.atguigu.gulimall.ware.service.WareInfoService;
@@ -29,6 +26,13 @@ import com.atguigu.common.utils.R;
 public class WareInfoController {
     @Autowired
     private WareInfoService wareInfoService;
+
+    //获取运费信息
+    @GetMapping("/fare")
+    public R getFare(@RequestParam("addrId") Long addrId){
+        FareVo fare = wareInfoService.getFare(addrId);
+        return R.ok().setData(fare);
+    }
 
     /**
      * 列表
