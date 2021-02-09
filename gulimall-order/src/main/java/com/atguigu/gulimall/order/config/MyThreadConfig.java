b79157cbae69afe43181a511e53a3fddc8cbc7d9
zmengyuan <zhangmy5@asiainfo.com>
2021-02-09 17:47:58 +0800

    [add] 订单服务 262 整合springsession 页面登录用户

---
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/config/MyThreadConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.gulimall.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+//如果ThreadPoolConfigProperties.class类没有加上@Component注解，那么我们在需要的配置类里开启属性配置的类加到容器中
+//@EnableConfigurationProperties(ThreadPoolConfigProperties.class)
+@Configuration
+public class MyThreadConfig {
+    @Bean
+    public ThreadPoolExecutor threadPoolExecutor(ThreadPoolConfigProperties pool){
+        return new ThreadPoolExecutor(pool.getCore(),
+                pool.getMaxSize(),
+                pool.getKeepAliveTime(),
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(100000),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
+    }
+}
