8d8386f715bb492cbf4e2b2fa1de5bd0b340c65a
zmengyuan <zhangmy5@asiainfo.com>
2021-02-09 14:06:22 +0800

    [add] 消息队列 单元测试添加交换机、队列、绑定

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -1,8 +1,22 @@
 package com.atguigu.gulimall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 使用rabbitmq
+ * 1、引入amqp场景，RabbitAutoConfiguration就会自动生效
+ * 2、给容器中自动配置了RabbitTemplate、AmqpAdmin、CachingConnectionFactory、RabbitMessagingTemplate
+ *      所有的属性都是通过
+ @ConfigurationProperties(
+ prefix = "spring.rabbitmq"
+ )
+ public class RabbitProperties绑定
+ 3、给配置文件中配置spring.rabbitmq
+ * 3、@EnableRabbit
+ */
+@EnableRabbit
 @SpringBootApplication
 public class GulimallOrderApplication {
 
--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,47 @@
-//package com.atguigu.gulimall.order;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class GulimallOrderApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.atguigu.gulimall.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class GulimallOrderApplicationTests {
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
+    /**
+     * 如何创建Exchange、queue、binding
+     *  使用AmqpAdmin
+     * 如何收发消息
+     */
+    @Test
+    public void contextLoads() {
+        DirectExchange directExchange = new DirectExchange("hello-java-exchange",true,false);
+        amqpAdmin.declareExchange(directExchange);
+        log.info("Exchange[{}]创建成功","hello-java-exchange");
+    }
+
+    @Test
+    public void createQueue(){
+        Queue queue = new Queue("hello-java-queue",true,false,false);
+        amqpAdmin.declareQueue(queue);
+        log.info("Queue[{}]创建成功","hello-java-queue");
+    }
+
+    @Test
+    public void createBinding(){
+        Binding binding = new Binding("hello-java-queue", Binding.DestinationType.QUEUE,"hello-java-exchange","hello.java",null);
+        amqpAdmin.declareBinding(binding);
+        log.info("Binding[{}]创建成功","hello-java-binding");
+    }
+
+}
