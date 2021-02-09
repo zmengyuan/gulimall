d766748a6265062fada5ad435a12a4862fbb80de
zmengyuan <zhangmy5@asiainfo.com>
2021-02-09 14:08:31 +0800

    [add] 消息队列 单元测试发送消息

--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -42,6 +43,21 @@ public class GulimallOrderApplicationTests {
         Binding binding = new Binding("hello-java-queue", Binding.DestinationType.QUEUE,"hello-java-exchange","hello.java",null);
         amqpAdmin.declareBinding(binding);
         log.info("Binding[{}]创建成功","hello-java-binding");
+    }
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+
+    /**
+     *  发送消息
+     */
+    @Test
+    public void sendMessageTest(){
+        //1、发送消息，
+        String msg = "hello word";
+        rabbitTemplate.convertAndSend("hello-java-exchange","hello.java",msg);
+        log.info("消息发送完成{}",msg);
     }
 
 }
