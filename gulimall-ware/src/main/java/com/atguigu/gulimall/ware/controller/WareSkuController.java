ff3b558aa6c0bb405718b3734c9cab3685e5764d
zmengyuan <zhangmy5@asiainfo.com>
2021-02-15 19:51:02 +0800

    [modify]删除商品库存的直接增删改

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareSkuController.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareSkuController.java
@@ -68,45 +68,4 @@ public class WareSkuController {
         return R.ok().put("page", page);
     }
 
-
-    /**
-     * 信息
-     */
-    @RequestMapping("/info/{id}")
-    public R info(@PathVariable("id") Long id){
-		WareSkuEntity wareSku = wareSkuService.getById(id);
-
-        return R.ok().put("wareSku", wareSku);
-    }
-
-    /**
-     * 保存
-     */
-    @RequestMapping("/save")
-    public R save(@RequestBody WareSkuEntity wareSku){
-		wareSkuService.save(wareSku);
-
-        return R.ok();
-    }
-
-    /**
-     * 修改
-     */
-    @RequestMapping("/update")
-    public R update(@RequestBody WareSkuEntity wareSku){
-		wareSkuService.updateById(wareSku);
-
-        return R.ok();
-    }
-
-    /**
-     * 删除
-     */
-    @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] ids){
-		wareSkuService.removeByIds(Arrays.asList(ids));
-
-        return R.ok();
-    }
-
 }
